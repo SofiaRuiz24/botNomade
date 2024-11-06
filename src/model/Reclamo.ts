dec06ff19194eeca4ee83c57d15dbac4b842b2eb
Sofia Ruiz <sbruizrocha@gmail.com>
2024-11-06 13:15:53 -0300

    Agregando conexion con BD Mongo Atlas

---
+++ src/model/Reclamo.ts
@@ -0,0 +1,33 @@
+import exp from "constants";
+import mongoose from "mongoose";
+
+export interface Reclamo {
+    id: string;
+    name: string;
+    docType: string;
+    docNumber: string;
+    phone: string;
+    email: string;
+    adress: string;
+    descriptionRec: string;
+    dateRec: Date;
+    estado: string;
+    usuario: string;
+}
+
+const reclamoSchema = new mongoose.Schema({
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    docType: { type: String, required: true },
+    docNumber: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
+    adress: { type: String, required: true },
+    descriptionRec: { type: String, required: true },
+    dateRec: { type: Date, required: false },
+    estado: { type: String, required: false },
+    usuario: { type: String, required: true },
+});
+
+const ReclamoModel = mongoose.model<Reclamo>('Reclamo', reclamoSchema);
+export default ReclamoModel;
--- src/templates/reclamoFlow.ts
+++ src/templates/reclamoFlow.ts
@@ -1,11 +1,13 @@
 import { addKeyword, EVENTS } from "@builderbot/bot";
+import { crearReclamo } from "~/controller/reclamoController";
+import { Reclamo } from "~/model/Reclamo";
 
 const reclamoFlow = addKeyword(EVENTS.ACTION)
-    .addAnswer('¿Desea iniciar una solicitud para su reclamo?', { capture: true, buttons: [{body:'Sí, quiero.'}, {body:'No, por el momento.'}] },
+    .addAnswer('¿Desea iniciar una solicitud para su reclamo?', { capture: true, buttons: [{ body: 'Sí, quiero.' }, { body: 'No, por el momento.' }] },
         async (ctx, ctxFn) => {
-            if(ctx.body === 'No, por el momento.'){
+            if (ctx.body === 'No, por el momento.') {
                 return ctxFn.endFlow('La solicitud ha sido cancelado. Puede realizar un reclamo en cualquier momento.')
-            } else if(ctx.body === 'Sí, quiero.'){
+            } else if (ctx.body === 'Sí, quiero.') {
                 return ctxFn.flowDynamic('Perfecto, voy a proceder a hacerte algunas preguntas.')
             } else {
                 return ctxFn.fallBack('No entiendo tu respuesta.')
@@ -13,46 +15,66 @@ const reclamoFlow = addKeyword(EVENTS.ACTION)
         })
     .addAnswer('Nombre y apellido del solicitante:', { capture: true },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({name: ctx.body})
+            await ctxFn.state.update({ name: ctx.body })
         }
     )
-    .addAnswer('Tipo de Documento:', { capture: true , buttons: [{body:'DNI'}, {body:'Pasaporte'}, {body:'Cédula de Identidad'}]},
+    .addAnswer('Tipo de Documento:', { capture: true, buttons: [{ body: 'DNI' }, { body: 'Pasaporte' }, { body: 'Cédula de Identidad' }] },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({docType: ctx.body})
+            await ctxFn.state.update({ docType: ctx.body })
         }
     )
     .addAnswer('Número de Documento:', { capture: true },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({docNumber: ctx.body})
+            await ctxFn.state.update({ docNumber: ctx.body })
         }
     )
     .addAnswer('Número de teléfono de contacto:', { capture: true },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({phone: ctx.body})
+            await ctxFn.state.update({ phone: ctx.body })
         }
     )
     .addAnswer('Correo electrónico:', { capture: true },
         async (ctx, ctxFn) => {
             const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-            if(!emailRegex.test(ctx.body)){
+            if (!emailRegex.test(ctx.body)) {
                 return ctxFn.fallBack('El correo electrónico ingresado no es válido. Por favor, ingresalo nuevamente.')
             }
-            await ctxFn.state.update({email: ctx.body})
+            await ctxFn.state.update({ email: ctx.body })
         }
     )
     .addAnswer('Dirección del solicitante:', { capture: true },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({address: ctx.body})
+            await ctxFn.state.update({ address: ctx.body })
         }
     )
     .addAnswer('Los datos del solicitante han sido cargados exitosamente. Ahora continuaremos con el reclamo, proporciona una descripción del mismo:, ', { capture: true },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({descriptionRec: ctx.body}) 
-    })
+            await ctxFn.state.update({ descriptionRec: ctx.body })
+        })
     .addAnswer('Ingrese la fecha en la que ocurrió el reclamo:', { capture: true },
         async (ctx, ctxFn) => {
-            await ctxFn.state.update({dateRec: ctx.body})
-            return ctxFn.flowDynamic('¡Gracias por tu tiempo! Tu reclamo ha sido registrado con éxito. Si necesita puede agregar una imagen o archivo relacionado con el reclamo.')
+            await ctxFn.state.update({ dateRec: ctx.body })
+
+            const reclamoData: Reclamo = {
+                id: ctxFn.state.get("id"), // Puedes asignar un ID generado o único aquí
+                name: ctxFn.state.get("name"),
+                docType: ctxFn.state.get("docType"),
+                docNumber: ctxFn.state.get("docNumber"),
+                phone: ctxFn.state.get("phone"),
+                email: ctxFn.state.get("email"),
+                adress: ctxFn.state.get("adress"),
+                descriptionRec: ctxFn.state.get("descriptionRec"),
+                dateRec: ctxFn.state.get("dateRec"),
+                estado: 'Pendiente',
+                usuario: ctx.from // Extrae el usuario o asigna el identificador adecuado
+            };
+
+            const resultado = await crearReclamo(reclamoData);
+            if (resultado) {
+                return ctxFn.flowDynamic('¡Gracias por tu tiempo! Tu reclamo ha sido registrado con éxito. Si necesita puede agregar una imagen o archivo relacionado con el reclamo.');
+            } else {
+                return ctxFn.flowDynamic('Hubo un problema al registrar tu reclamo. Por favor, intenta nuevamente más tarde.');
+            }
         }
     )
-    export { reclamoFlow };
+export { reclamoFlow };
