322006e85dfcc5a955362402acc812daa534a555
Sofia <sbruizrocha@gmail.com>
2025-07-03 19:44:53 +0000

    devulelta al import anterior

--- src/services/autoReclamo.ts
+++ src/services/autoReclamo.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import qs from "qs";
 import { Reclamo } from "~/model/Reclamo";
 import { config } from "~/config";
